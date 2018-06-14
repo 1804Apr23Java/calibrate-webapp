200ef3fdf908efb860d51738791f891d11c9771a
jlmunoz44 <jlmunoz4.miners@gmail.com>
2018-06-14 09:01:55 -0400

    library list service

--- src/app/components/library-list/library-list.component.ts
+++ src/app/components/library-list/library-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LibraryListService } from '../../services/library-list.service';
+import { Library } from '../../classes/library';
 
 @Component({
   selector: 'app-library-list',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./library-list.component.css']
 })
 export class LibraryListComponent implements OnInit {
+  public libraryList: Library[];
 
-  constructor() { }
+  constructor(private libraryService: LibraryListService) { }
 
   ngOnInit() {
+    this.getAllLibrary();
   }
+  getAllLibrary(): void {
+  this.libraryService.getLibrary().subscribe(
+    (libraryList: Library[]) => {
+      this.libraryList = libraryList;
+    },
+    error => console.log(`Error: ${error}`)
+  );
+}
 
 }
+/*  getAccountById(id: number): void {
+    this.accountService.getAccountById(id).subscribe(
+      (account: Account) => {
+        this.account = account;
+        this.acString = JSON.stringify(this.account);
+      },
+      error => console.log(`Error: ${error}`)
+    );
+  }*/
