e18f12b63318e05a6f26c1dac6fb1bb412ffb861
jlmunoz44 <jlmunoz4.miners@gmail.com>
2018-06-14 14:19:58 -0400

    added some comments to help nav quiz components

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Library } from '../classes/library';
+import { Quiz } from '../classes/quiz';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  private accountUrl = 'http://ec2-184-72-131-208.compute-1.amazonaws.com:8080/CalibrateBackend/quiz/5';
+
+  public getPublicLibraries(): Observable<Quiz> {
+    return this.httpClient.get<Quiz>(this.accountUrl);
+  }
 }
